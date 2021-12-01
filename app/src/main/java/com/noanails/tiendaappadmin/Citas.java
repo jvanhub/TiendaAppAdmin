340502339b2ff0682a76eeccd0a6b8613853f08e
jvanhub <ivan.m.diaz@hotmail.es>
2021-12-01 10:51:44 +0100

    Nueva v1

--- app/src/main/java/com/noanails/tiendaappadmin/Citas.java
+++ app/src/main/java/com/noanails/tiendaappadmin/Citas.java
@@ -1,12 +1,26 @@
 package com.noanails.tiendaappadmin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import clasesObjeto.ListAdapterUsuarios;
@@ -19,36 +33,27 @@ public class Citas extends AppCompatActivity {
     private String uId = "";
     private String id;
     private String idCita;
-    ListAdapterUsuarios listAdapterUsuarios;
+    ListAdapterCitas listAdapterCitas;
     FirebaseUser mAuth;
     DatabaseReference mDatabase;
-    List<ListElement_Usuarios> elements;
+    List<ListElement_Citas>elements;
     private String servicio="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_citas);
-/*
+
         mAuth = FirebaseAuth.getInstance().getCurrentUser();
         mDatabase = FirebaseDatabase.getInstance().getReference();
         id = mAuth.getUid();
         Button verCita = (Button) findViewById(R.id.buttonVerCitas);
-        Button verCitaActual = (Button) findViewById(R.id.buttonVerCitasActules);
         Button volver = (Button) findViewById(R.id.buttonVolver2);
         elements = new ArrayList<>();
 
         verCita.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                recogerCitas();
-            }
-        });
-
-        verCitaActual.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                recogerCitasActualizadas();
-            }
+                recogerCitas();            }
         });
 
         volver.setOnClickListener(new View.OnClickListener() {
@@ -59,48 +64,8 @@ public class Citas extends AppCompatActivity {
         });
     }
 
-    //Se encarga de recger, comparar e insertar los datos junto con los elementos.
-    public void recogerCitas() {
-        mDatabase.child("Reservas").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                elements.clear();
-
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    try {
-                        servicioBBDD = snapshot.child("servicio").getValue().toString();
-                        fechaBBDD = snapshot.child("fecha").getValue().toString();
-                        horaBBDD = snapshot.child("hora").getValue().toString();
-                        uId = snapshot.child("uId").getValue().toString();
-
-                        if (uId.equals(id)) {
-                            insertElements();
-                        }
-                    } catch (NullPointerException n) {
-                        Toast.makeText(Citas.this, "No hay citas pendientes", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Toast.makeText(Citas.this, "Error BBDD", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    //Método encargado de crear e introducir los datos en cada elemento.
-    public void insertElements() {
-        elements.add(new ListElemnt_Usuarios(servicioBBDD, fechaBBDD, horaBBDD, null));
-        ListAdapterUsuarios listAdapterUsuarios = new ListAdapterUsuarios(elements, this);
-        RecyclerView recyclerView = findViewById(R.id.listRecyclerView);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(listAdapterUsuarios);
-    }
-
     //Metodo para recoger todas las fechas desde el día de hoy incuido, las fechas pasadas no.
-    public void recogerCitasActualizadas() {
+    public void recogerCitas() {
         mDatabase.child("Reservas").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -121,7 +86,7 @@ public class Citas extends AppCompatActivity {
                         idCita = snapshot.getKey();
                         if (uId.equals(id)) {
                             if(((Integer.parseInt(extractFecha[2]) - anyo) >= 0) && ((Integer.parseInt(extractFecha[1]) - mes) >= 0) && ((Integer.parseInt(extractFecha[0]) - dia) >= 0)){
-                                insertElementsActual();
+                                insertElements();
                             }
                         }
                     }
@@ -138,13 +103,12 @@ public class Citas extends AppCompatActivity {
     }
 
     //Método encargado de crear e introducir los datos en cada elemento.
-    public void insertElementsActual() {
-        elements.add(new ListElemnt_Usuarios(servicioBBDD,fechaBBDD, horaBBDD, idCita));
-        listAdapterUsuarios = new ListAdapter2(elements, this);
+    public void insertElements() {
+        elements.add(new ListElement_Citas(servicioBBDD,fechaBBDD, horaBBDD, idCita));
+        listAdapterCitas = new ListAdapterCitas(elements, this);
         RecyclerView recyclerView = findViewById(R.id.listRecyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(listAdapterUsuarios);
-    }*/
+        recyclerView.setAdapter(listAdapterCitas);
     }
 }
---
+++ app/src/main/java/com/noanails/tiendaappadmin/ListAdapterCitas.java
@@ -0,0 +1,107 @@
+package com.noanails.tiendaappadmin;
+
+import static androidx.core.content.ContextCompat.startActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.List;
+
+//Esta clase relaciona la parte grafica con los datos (Fecha y Hora) que vamos ha tratar.
+public class ListAdapterCitas extends RecyclerView.Adapter<ListAdapterCitas.ViewHolder> {
+
+    private List<ListElement_Citas>mData;//Esta lista tiene todos los datos de ListElement.
+    private LayoutInflater mInflater;//Describe de que archivo proviene.
+    public Context context;//Define de que clase estamos llamando este adaptador.
+
+    //Constructor.
+    public ListAdapterCitas(List<ListElement_Citas> itemList, Context context){
+        this.mInflater = LayoutInflater.from(context);
+        this.context=context;
+        this.mData = itemList;
+    }
+    public ListAdapterCitas(){
+    }
+
+    //A continuación damos la referencia de como se va a ver cada "tarjeta"
+    @NonNull
+    @Override
+    public ListAdapterCitas.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.list_element_citas, null);
+        return new ViewHolder(view);
+    }
+
+    //
+    @Override
+    public void onBindViewHolder(@NonNull ListAdapterCitas.ViewHolder holder, int position) {
+        holder.bindData(mData.get(position));
+    }
+
+    //Método que devuelve el tamaño de elementos que hay en la lista.
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    //Este metodo sirve para redefinir los elementos de la lista.
+    public void setItems(List<ListElement_Citas> items){
+        mData=items;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        TextView mostrar, fecha, hora, servicio;
+        Button btMod, btElim;
+        DatabaseReference dbr;
+
+
+        ViewHolder(View itemView){
+            super(itemView);
+            dbr = FirebaseDatabase.getInstance().getReference();
+            mostrar = itemView.findViewById(R.id.textViewCitasCard2);
+            fecha = itemView.findViewById(R.id.textViewFechaCard2);
+            hora = itemView.findViewById(R.id.textViewHoraCard2);
+            servicio = itemView.findViewById(R.id.textViewServicio2);
+            btMod = itemView.findViewById(R.id.buttonModifi);
+            btElim = itemView.findViewById(R.id.buttonDelet);
+        }
+
+        void bindData(final ListElement_Citas item){
+            fecha.setText(item.getFecha());
+            hora.setText(item.getHora());
+            servicio.setText(item.getServicio());
+            btElim.setContentDescription(item.getIdCita());
+
+            btElim.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dbr.child("Reservas").child(btElim.getContentDescription().toString()).removeValue();
+                    Toast.makeText(v.getContext(), "La cita se ha eliminado correctamente.", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(v.getContext(),Citas.class);
+                    v.getContext().startActivity(intent);
+                }
+            });
+
+            btMod.setContentDescription(item.getIdCita());
+            btMod.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(v.getContext(),Modificar_Citas.class);
+                    intent.putExtra("boton",btMod.getContentDescription());
+                    v.getContext().startActivity(intent);
+                }
+            });
+        }
+    }
+}
